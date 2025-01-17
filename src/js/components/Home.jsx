82c7ee29c8ecbe0637a123369b38fa5956cc5479
grisottoc <sales.mercadodirecto@gmail.com>
2025-01-17 21:24:15 +0000

    GrisottoUpdate

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React from 'react';
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
+import Card from './Card';
+import '../../styles/index.css'; // Import the CSS file with the correct path
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <div className="card-container">
+        <Card
+          title="Card title 1"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          imgSrc="https://via.placeholder.com/500x325"
+        />
+        <Card
+          title="Card title 2"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          imgSrc="https://via.placeholder.com/500x325"
+        />
+        <Card
+          title="Card title 3"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          imgSrc="https://via.placeholder.com/500x325"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <div className="navbar-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
